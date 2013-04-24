efd83d34e39ca843babe9a873e5afb129afe2af5
Chris Jee <cjee@etsy.com>
2013-04-24 18:28:49 +0000

    Add vsqlrc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim .tmux.conf .ackrc"    # list of files/folders to symlink in homedir
+files=".bashrc .vimrc .vim .tmux.conf .ackrc .vsqlrc"    # list of files/folders to symlink in homedir
 
 ##########
 

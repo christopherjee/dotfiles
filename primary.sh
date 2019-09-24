7d039b0c611e18cbc5a9fdaae2a7532eb47a120d
Christopher Jee <chris@christopherjee.com>
2019-09-23 22:01:52 -0400

    Startup script for Primary with tmux

---
+++ primary.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+tmux new-session -A -s primary -d -c ~/dev/primary
+
+# maybe need to attach at the end
+
+tmux select-window -t 0
+tmux rename-window web-dev
+tmux send-keys "sudo docker-compose start web-dev && sudo docker-compose start dev-db && sudo docker-compose start redis" C-m
+
+tmux new-window -c ~/dev/primary
+tmux select-window -t 1
+tmux rename-window web-dev-logs
+tmux send-keys "sudo docker-compose logs --follow web-dev" C-m
+
+tmux new-window -c ~/dev/primary
+tmux select-window -t 2
+tmux rename-window bash
+tmux send-keys "git branch; git status" C-m
+
+tmux new-window -c ~/dev/primary
+tmux select-window -t 3
+tmux rename-window vim
+tmux send-keys "vim" C-m
+
+tmux attach-session -t primary

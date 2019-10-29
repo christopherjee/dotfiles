e043d062daafe6cb7782ce05e46345f8398c67dd
Christopher Jee <chris@christopherjee.com>
2019-10-28 22:57:05 -0400

    Check for session before creating one

--- faos.sh
+++ faos.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-tmux new-session -A -s faos -d -c ~/dev/faos
+if ! tmux attach-session -t faos
+then
+    tmux new-session -A -s faos -d -c ~/dev/faos-v0
 
-tmux select-window -t 0
-tmux rename-window yarn
-tmux send-keys "yarn start" C-m
+    tmux select-window -t 0
+    tmux rename-window yarn
+    tmux send-keys "slate start" C-m
 
-tmux new-window -c ~/dev/faos
-tmux select-window -t 1
-tmux rename-window bash
-tmux send-keys "git branch; git status" C-m
+    tmux new-window -c ~/dev/faos-v0
+    tmux select-window -t 1
+    tmux rename-window bash
+    tmux send-keys "git branch; git status" C-m
 
-tmux new-window -c ~/dev/faos
-tmux select-window -t 2
-tmux rename-window vim
-tmux send-keys "vim" C-m
+    tmux new-window -c ~/dev/faos-v0
+    tmux select-window -t 2
+    tmux rename-window vim
+    tmux send-keys "vim" C-m
 
-tmux attach-session -t faos
+    tmux attach-session -t faos
+fi
--- primary.sh
+++ primary.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-tmux new-session -A -s primary -d -c ~/dev/primary
+if ! tmux attach-session -t primary
+then
+    tmux new-session -A -s primary -d -c ~/dev/primary
 
-tmux select-window -t 0
-tmux rename-window web-dev
-tmux send-keys "docker-compose start web-dev && docker-compose start dev-db && docker-compose start redis" C-m
+    tmux select-window -t 0
+    tmux rename-window web-dev
+    tmux send-keys "docker-compose start web-dev && docker-compose start dev-db && docker-compose start redis" C-m
 
-tmux new-window -c ~/dev/primary
-tmux select-window -t 1
-tmux rename-window web-dev-logs
-tmux send-keys "docker-compose logs --follow web-dev" C-m
+    tmux new-window -c ~/dev/primary
+    tmux select-window -t 1
+    tmux rename-window web-dev-logs
+    tmux send-keys "docker-compose logs --follow web-dev" C-m
 
-tmux new-window -c ~/dev/primary
-tmux select-window -t 2
-tmux rename-window bash
-tmux send-keys "git branch; git status" C-m
+    tmux new-window -c ~/dev/primary
+    tmux select-window -t 2
+    tmux rename-window bash
+    tmux send-keys "git branch; git status" C-m
 
-tmux new-window -c ~/dev/primary
-tmux select-window -t 3
-tmux rename-window vim
-tmux send-keys "vim" C-m
+    tmux new-window -c ~/dev/primary
+    tmux select-window -t 3
+    tmux rename-window vim
+    tmux send-keys "vim" C-m
 
-tmux attach-session -t primary
+    tmux attach-session -t primary
+fi

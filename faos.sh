bd29dde5fad8f9a5250c951a60cefcec39d74674
Christopher Jee <chris@christopherjee.com>
2019-11-12 23:19:13 -0500

    Add dotfiles to faos.sh and do some renaming

--- faos.sh
+++ faos.sh
@@ -2,21 +2,26 @@
 
 if ! tmux attach-session -t faos
 then
-    tmux new-session -A -s faos -d -c ~/dev/faos-v0
+    tmux new-session -A -s faos -d -c ~/dev/faos
 
     tmux select-window -t 0
-    tmux rename-window slate
-    tmux send-keys "slate start" C-m
+    tmux rename-window yarn
+    tmux send-keys "yarn deploy" C-m
 
-    tmux new-window -c ~/dev/faos-v0
+    tmux new-window -c ~/dev/faos
     tmux select-window -t 1
-    tmux rename-window bash
+    tmux rename-window git
     tmux send-keys "git branch; git status" C-m
 
-    tmux new-window -c ~/dev/faos-v0
+    tmux new-window -c ~/dev/faos
     tmux select-window -t 2
     tmux rename-window vim
     tmux send-keys "vim" C-m
+
+    tmux new-window -c ~/dotfiles
+    tmux select-window -t 2
+    tmux rename-window dotfiles
+    tmux send-keys "git branch; git status" C-m
 
     tmux attach-session -t faos
 fi
